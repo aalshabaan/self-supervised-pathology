ca32888b18fb594a54b60f7ee8e81a37d062ca46
AbedShabaan <abed.shabaan@epfl.ch>
2022-05-30 14:29:32 +0200

    Add mpp to roi dfs

---
+++ add_mpp.py
@@ -0,0 +1,17 @@
+import os
+
+import Abed_utils
+
+import pandas as pd
+from glob import glob
+from tqdm import tqdm
+
+from wsi import WholeSlideDataset
+
+for csv in tqdm(glob(os.path.join(Abed_utils.OUTPUT_ROOT, 'roi_detections'))):
+    bname = os.path.basename(csv).split('_roi')[0]
+    dataset = WholeSlideDataset(os.path.join(Abed_utils.BERN_COHORT_ROOT, bname))
+    df = pd.read_csv(csv)
+    df['mpp'] = dataset.mpp
+
+    df.to_csv(csv, index=False)
