eb419972a7a69f21a375064aa2d1fc73555b5468
AbedShabaan <abed.shabaan@epfl.ch>
2022-05-30 14:37:08 +0200

    Typo

--- add_mpp.py
+++ add_mpp.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 
 from wsi import WholeSlideDataset
 
-for csv in tqdm(glob(os.path.join(Abed_utils.OUTPUT_ROOT, 'roi_detections'))):
+for csv in tqdm(glob(os.path.join(Abed_utils.OUTPUT_ROOT, 'ROI_detections'))):
     bname = os.path.basename(csv).split('_roi')[0]
     dataset = WholeSlideDataset(os.path.join(Abed_utils.BERN_COHORT_ROOT, bname))
     df = pd.read_csv(csv)
