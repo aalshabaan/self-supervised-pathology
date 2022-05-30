ea611b1be2004c4787e2462ae90fb1dd2b5a373b
AbedShabaan <abed.shabaan@epfl.ch>
2022-05-30 14:56:16 +0200

    Removed leading zeros

--- add_mpp.py
+++ add_mpp.py
@@ -12,8 +12,8 @@ from wsi import WholeSlideDataset
 if __name__ == '__main__':
     for csv in tqdm(glob(os.path.join(Abed_utils.OUTPUT_ROOT, 'ROI_detections', '*.csv'))):
         bname = os.path.basename(csv).split('_roi')[0]
-        dir_name = re.match(r'^\d+', bname)[0]
-        print(dir_name)
+        dir_name = str(int(re.match(r'^\d+', bname)[0]))
+        # print(dir_name, bname)
         dataset = WholeSlideDataset(os.path.join(Abed_utils.BERN_COHORT_ROOT, dir_name, bname))
         df = pd.read_csv(csv)
         df['mpp'] = dataset.mpp
